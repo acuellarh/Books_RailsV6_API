9d9e11d7b5f2761467bbf8a49555d24f98781536
Alejandro <acuellarh@gmail.com>
2023-08-12 08:19:11 -0500

    Video 5, Active records validations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     if book.save
       render json: book, status: :created
     else
-      render json: book.errors, status: :unproccessable_entity
+      render json: book.errors, status: :unprocessable_entity
     end    
   end
 
@@ -22,32 +22,4 @@ class BooksController < ApplicationController
 end
 
 
-# class BooksController < ApplicationController
-#   def index
-#     render json: Book.all
-#   end
 
-#   def create
-#     if valid_book_params?
-#       book = Book.new(book_params)
-
-#       if book.save
-#         render json: book, status: :created
-#       else
-#         render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
-#       end
-#     else
-#       render json: { errors: ["Both title and author parameters are required."] }, status: :unprocessable_entity
-#     end
-#   end
-
-#   private
-
-#   def book_params
-#     params.require(:book).permit(:title, :author)
-#   end
-
-#   def valid_book_params?
-#     book_params[:title].present? && book_params[:author].present?
-#   end
-# end
