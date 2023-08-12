07cb5e0b07f9f44e2c19700d52847df446450da2
Alejandro <acuellarh@gmail.com>
2023-08-11 19:55:59 -0500

    Improved version commented, add a validation to ensure title and author parameter to create the new register

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,3 +20,34 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :author)    
   end
 end
+
+
+# class BooksController < ApplicationController
+#   def index
+#     render json: Book.all
+#   end
+
+#   def create
+#     if valid_book_params?
+#       book = Book.new(book_params)
+
+#       if book.save
+#         render json: book, status: :created
+#       else
+#         render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
+#       end
+#     else
+#       render json: { errors: ["Both title and author parameters are required."] }, status: :unprocessable_entity
+#     end
+#   end
+
+#   private
+
+#   def book_params
+#     params.require(:book).permit(:title, :author)
+#   end
+
+#   def valid_book_params?
+#     book_params[:title].present? && book_params[:author].present?
+#   end
+# end
