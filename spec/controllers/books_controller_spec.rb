6b8f20ba8986383de6cdc3c514e4202cc665c96e
Alejandro <acuellarh@gmail.com>
2023-08-21 12:49:05 -0500

    Video 17, Testing Active Job with RSpec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -2,10 +2,23 @@ require "rails_helper"
 
 RSpec.describe Api::V1::BooksController, type: :controller do
 
-  it 'has a max limit of 100' do
-    expect(Book).to receive(:limit).with(100).and_call_original
-
-   get :index, params: { limit: 999 }
-  end
+  describe 'get index' do    
+    it 'has a max limit of 100' do
+      expect(Book).to receive(:limit).with(100).and_call_original
   
+     get :index, params: { limit: 999 }
+    end    
+  end
+
+  describe 'Post create' do
+    let(:book_name) {'Harry potter'}
+
+    it 'calls UpdateSkuJob with correct params' do
+      post :create, params: {
+        author: {first_name: 'JK', last_name: 'Rowling', age: 48},
+        book: {title: book_name}
+      }
+    end
+  end
+
 end
